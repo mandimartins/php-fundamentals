fc8661f259fca9102562e8344731f2666ad2f394
mandimartins <mandimartins23@gmail.com>
2021-10-29 20:06:13 -0300

    basic string functions

---
+++ string_functions.php
@@ -0,0 +1,29 @@
+<?php
+
+    # substr()
+    # Returns a portion of a string
+
+    $output = substr("Hello", 1); // start off by the 2º letter => the number is actually a index like array
+
+    // echo $output; // prints out ello
+
+    #Returns the length of a string
+    $strlen("Hello World!");
+
+    #Finds the position of the first occurence of a sub string
+    strpos("Hello World!", "o"); //prints out 4
+
+    #Finds the position of the last occurence of a sub string
+    strrpos("Hello World!", "o"); //prints out 7
+
+    #trim()
+    #Strips whitespace
+
+    #strtoupper("My simple string")
+    #Makes everything uppercase
+
+    #ucwords()
+    #Capitalize every word
+
+    #str_replace()
+    #Replace all occurences of a search string with a replacement
