34c218ada27ea35fdf3e24f4f05c25d7fa4e6736
mandimartins <mandimartins23@gmail.com>
2021-11-01 12:06:42 -0300

    OOP on PHP

--- OOP/index.php
+++ OOP/index.php
@@ -6,14 +6,75 @@ class Person
         #public => acess from anywhere outside the class
         #private => acess only within this class
         #protected => access from this class and any other that extends it
-    public $name;
-    public $email;
+    private $name;
+    private $email;
+
+    //It does not need to be instantiated to use.
+    private static $ageLimit = 50;
+
+    //Run when instantiate the object
+    public function __construct($name, $email)
+    {
+        $this->name = $name;
+        $this->$email = $email;
+        echo __CLASS__ . " created!" ;
+    }
+
+    public function __destruct()
+    {
+        echo __CLASS__ . " destroyed!";
+    }
+
+    // good for validations, is more secure.
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public static function getAgeLimit()
+    {
+        return  self::$ageLimit;
+    }
 }
 
-//Instantiate a class / Create an object
-$person1 = new Person();
+echo Person::getAgeLimit();
+#Instantiate a class / Create an object
+// $person1 = new Person("John Doe", "john@gmail.com");
 
-//This it not a good practice
-$person1->name = "Agnes Obel";
+// $person1->getName();
 
-echo $person1->name;
+#This it not a good practice
+// $person1->name = "Agnes Obel";
+
+// echo $person1->name;
+
+class Customer extends Person
+{
+
+    private $balance;
+    public function __construct($name, $email, $balance)
+    {
+        parent::__construct($name, $email, $balance);
+        $this->balance = $balance;
+        echo "A new " . __CLASS__ . " has been created!<br>";
+    }
+
+    public function setBalance($balance)
+    {
+        $this->balance = $balance;
+    }
+
+    public function getBalance()
+    {
+        return $this->balance;
+    }
+}
+
+$customer = new Customer("John Doe", "John123@gmail.com", 3000);
+echo $customer->getName() . "<br>";
+echo $customer->getBalance() . "<br>";
